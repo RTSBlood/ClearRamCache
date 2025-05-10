f96efa71f6de9c7fe48b6fc40e4894ed2bfbdd04
RTSBlood <110573786+RTSBlood@users.noreply.github.com>
2025-05-10 02:21:01 +0200

    ClearRamCache.bat

--- ClearRamCache.bat
+++ ClearRamCache.bat
@@ -10,15 +10,15 @@ if '%errorlevel%' NEQ '0' (
 )
 
 :: Define the path of the executable
-set "FICHIER=%~dp0EmptyStandbyList.exe"
+set "FILE=%~dp0EmptyStandbyList.exe"
 set "URL=https://github.com/stefanpejcic/EmptyStandbyList/raw/refs/heads/master/EmptyStandbyList.exe"
 
 :: Check if the file exists, if not, download it
-if not exist "%FICHIER%" (
+if not exist "%FILE%" (
     echo The tool EmptyStandbyList.exe was not found, downloading...
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%FICHIER%')"
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%FILE%')"
 
-    if not exist "%FICHIER%" (
+    if not exist "%FILE%" (
         echo Download failed!
         exit /b 1
     )
@@ -27,7 +27,7 @@ if not exist "%FICHIER%" (
 
 :: Clear standby memory cache
 echo Clearing standby memory cache...
-"%FICHIER%" standbylist
+"%FILE%" standbylist
 if %errorlevel% NEQ 0 (
     echo Error while executing EmptyStandbyList.exe
     pause
